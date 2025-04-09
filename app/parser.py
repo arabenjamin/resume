068d9e318a3b9beaeefc18961019d56737395169
Ara Sheperdigian <ara.benjamin@gmail.com>
2025-04-09 13:48:57 -0700

    Added tests, updated parser, added github actions to run tests

---
+++ app/parser.py
@@ -0,0 +1,73 @@
+import json
+import yaml
+import logging
+
+from app.resume_model import Resume
+
+
+
+class Parser:
+    def __init__(self, json_path=None) -> None:
+        
+        self.json_path = json_path
+        if not json_path:
+            raise ValueError("JSON path cannot be None")
+        
+        if not json_path.endswith('resume.json'):
+            raise ValueError("File must be a JSON file")
+        
+
+        self.data = None
+        self.resume = None
+        self.__load_json()
+        
+
+    def __load_json(self) -> None:
+        with open(self.json_path, 'r') as f:
+            self.data = json.load(f)
+            self.resume = Resume(**self.data)
+
+    def __get_resume(self) -> Resume:
+        return self.resume
+    
+    def get_markdown(self):
+
+        with open("./resume.md", 'w') as f:
+
+            f.write(f"# {self.resume.name}\n\n")
+            f.write(f"**Phone:** {self.resume.contact.phone}  \n")
+            f.write(f"**Email:** {self.resume.contact.email}  \n")
+            f.write(f"**GitHub:** [{self.resume.contact.github}]({self.resume.contact.github})  \n")
+            f.write(f"**LinkedIn:** [{self.resume.contact.linkedin}]({self.resume.contact.linkedin})\n\n")
+            
+            f.write("## Objective\n\n")
+            f.write(f"{self.resume.objective}\n\n")
+            
+            f.write("## Experience\n\n")
+            for job in self.resume.experience:
+                f.write(f"### {job.company} ({job.location})\n")
+                f.write(f"**{job.title}**\n")
+                f.write(f"*{job.dates}*\n\n")
+                for responsibility in job.responsibilities:
+                    f.write(f"- {responsibility}\n")
+                f.write("\n")
+            
+            f.write("## Skills\n\n")
+            for skill_category, skills in self.resume.skills.model_dump().items():
+                if skills:
+                    f.write(f"### {skill_category.capitalize()}\n")
+                    for skill in skills:
+                        f.write(f"- {skill}\n")
+                    f.write("\n")
+
+
+
+
+if __name__ == "__main__":
+    JSON_PATH = '/home/ara/resume/resume.json'
+    MARKDOWN_PATH = '/home/ara/resume/resume.md'
+    YAML_PATH = '/home/ara/resume/resume.yaml'
+    print("Converting resume to Markdown...")
+    parser = Parser(json_path=JSON_PATH)
+    md_resume = parser.get_markdown()
+    print("Conversion complete!")
---
+++ tests/test_parser.py
@@ -0,0 +1,38 @@
+import pytest
+import os
+from app.parser import Parser
+from app.resume_model import Resume
+
+
+
+
+def test_parser_init(mock_json_file):
+    with pytest.raises(ValueError):
+        parser = Parser()
+
+    parser = Parser(json_path=str(mock_json_file))
+    assert parser is not None
+    assert isinstance(parser, Parser)
+    assert parser.data is not None
+    assert parser.resume is not None
+
+def test_load_json(mock_json_file):
+    parser = Parser(json_path=str(mock_json_file))
+    assert parser.data is not None
+    assert parser.resume is not None
+    assert isinstance(parser.resume, Resume)
+    assert 'name' in parser.data
+    assert 'contact' in parser.data
+    assert 'objective' in parser.data
+    assert 'experience' in parser.data
+    assert 'skills' in parser.data
+
+def test_get_markdown(mock_json_file):
+    file_path = str(mock_json_file)
+    parser = Parser(json_path=file_path)
+    parser.get_markdown()
+    assert parser.data is not None
+    assert parser.resume is not None
+    assert isinstance(parser.resume, Resume)
+
+    assert os.path.exists(file_path)
