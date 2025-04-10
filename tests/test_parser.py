ed78d258ce1671c550d6d4adc8105ae282319428
Ara Sheperdigian <ara.benjamin@gmail.com>
2025-04-09 19:23:17 -0700

    Updated tests to look for correct file

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -41,8 +41,8 @@ def test_get_markdown_saves_to_docs(mock_json_file):
     file_path = str(mock_json_file)
     parser = Parser(json_path=file_path)
     parser.get_markdown()
-    assert os.path.exists("./docs/resume.md")
-    with open("./docs/resume.md", 'r') as f:
+    assert os.path.exists("./docs/index.md")
+    with open("./docs/index.md", 'r') as f:
         content = f.read()
         assert "## Objective" in content
         assert "## Experience" in content
